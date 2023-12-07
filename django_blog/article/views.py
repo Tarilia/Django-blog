223ce23aacf31fac3b690e0110553ce9776f1369
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-07 14:20:12 +0300

    update views.py

--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'articles/index.html', context={
-            'name': 'django blog', })
+        return redirect(reverse('article', args=['python', 42]))
+
+
+class ArticleView(View):
+    def get(self, request, tags, article_id):
+        return render(request, 'articles/index.html',
+                      context={'article_id': article_id, 'tags': tags})
