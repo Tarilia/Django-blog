875f3de2b857c94f4fd0125a45775b1a72e38ca2
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-07 14:17:24 +0300

    update urls.py

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from django_blog.article.views import IndexView
+from django_blog.article.views import IndexView, ArticleView
 
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('<str:tags>/<int:article_id>/', ArticleView.as_view(), name='article'),
 ]
