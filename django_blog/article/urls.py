88dcece09644d409d86d95bd950c5b67fb97a80e
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-09 13:07:49 +0300

    add article id

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from django_blog.article.views import IndexView
+from django_blog.article.views import IndexView, ArticleView
 
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('<int:id>/', ArticleView.as_view()),
 ]
