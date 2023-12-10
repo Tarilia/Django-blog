61450babeeb68960b61d2211043f8f9b9e84b2c0
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2023-12-10 13:38:28 +0300

    add ArticleFormCreateView

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from django_blog.article.views import IndexView, ArticleView
+from django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
 
 
 urlpatterns = [
     path('', IndexView.as_view()),
     path('<int:id>/', ArticleView.as_view()),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django_blog.article.models import Article
+from django.forms import ModelForm
 
 
 class IndexView(View):
@@ -20,3 +21,23 @@ class ArticleView(View):
         return render(request, 'articles/show.html', context={
             'article': article,
         })
+
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['name', 'body']
+
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+        return render(request, 'articles/create.html', {'form': form})
